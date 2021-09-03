0ed08f063812fc004f8ad20b59c8f835d4261b9a
MOLOCH-dev <sabnisanushree@gmail.com>
2021-09-03 13:36:36 +0530

    crawling and added _site

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 echo "This is a bash shell"
 echo "Link $1"
-echo "::set-output name=orphan-pages::$1"
+export LOG=log.txt
+export URL=$1
+wget --no-directories --mirror --spider "$URL" 2>&1 | tee "$LOG";
+crawled_links=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`);
+echo "::set-output name=orphan-pages::$crawled_links"
