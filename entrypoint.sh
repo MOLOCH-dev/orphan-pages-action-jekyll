42ce40f2596da84a391e83cd2142622b13c54a0b
MOLOCH-dev <sabnisanushree@gmail.com>
2021-09-03 14:14:19 +0530

    Composite action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@ export URL=$1
 wget --no-directories --mirror --spider "$URL" 2>&1 | tee "$LOG";
 crawled_links=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`);
 echo "::set-output name=orphan-pages::$crawled_links"
+        # echo "::set-output name=orphan-pages::$(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`)"
